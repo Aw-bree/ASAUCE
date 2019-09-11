ce4115bbf61e8686464e3cf77a58e228e85aff7a
Aubrie Rice <aubrielrice@gmail.com>
2019-09-10 21:48:58 -0700

    Change seed file to also add product description and product photos 2 through 4

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,88 +9,62 @@
 require 'open-uri'
 require 'json'
 
-# Product.delete_all
+Product.delete_all
 
-# open("https://raw.githubusercontent.com/Aw-bree/seedHelper/master/csvjson.json") do |products|
-#   data = []
+open("https://raw.githubusercontent.com/Aw-bree/seedHelper/master/asauce_create_products.json") do |products|
+  data = []
 
-#   products.read.each_line do |product|
-#     @item = JSON.parse(product, :quirks_mode => true)
-#     object = {
-#       "product_code": @item["product_code"],
-#       "title": @item["title"],
-#       "model_size": @item["model_size"],
-#       "model_height": @item["model_height"],
-#       "fabric_stretch": @item["fabric_stretch"],
-#       "fabric_material":  @item["fabric_material"],
-#       "main_fiber_content":   @item["main_fiber_content"],
-#       "care_advice": @item["care_advice"],
-#       "care_instructions":  @item["care_instructions"],
-#       "price":  @item["price"],
-#       "markdown": @item["markdown"]
-#     }
-#     data << object
-#   end
-#   Product.create!(data)
-# end
+  products.read.each_line do |product|
+    @item = JSON.parse(product, :quirks_mode => true)
+    object = {
+      "product_code": @item["product_code"],
+      "title": @item["title"],
+      "model_size": @item["model_size"],
+      "model_height": @item["model_height"],
+      "fabric_stretch": @item["fabric_stretch"],
+      "fabric_material":  @item["fabric_material"],
+      "main_fiber_content":   @item["main_fiber_content"],
+      "care_advice": @item["care_advice"],
+      "care_instructions":  @item["care_instructions"],
+      "price":  @item["price"],
+      "markdown": @item["markdown"],
+      "description": @item["description"]
+    }
+    data << object
+  end
+  Product.create!(data)
+end
 
-# open("https://raw.githubusercontent.com/Aw-bree/seedHelper/master/file_paths.json") do |productfiles|
-#   data = []
-
-#   productfiles.read.each_line do |product|
-#     @item = JSON.parse(product, :quirks_mode => true)
-#     object = {
-#       "product_code": @item["product_code"],
-#       "img_path": @item["file_path"],
-#     }
-#     data << object
-#   end
-
-
-#   data.each do |prods|
-#     code = prods[:product_code]
-#     @product = Product.find_by(product_code: code)
-
-#     file = open("https://s3-us-west-1.amazonaws.com/asauce-seeds/#{prods[:img_path]}")
-    
-
-#     @product.photos.attach(io: file, filename: prods[:img_path])
-
-#     # file = open("https://s3.amazonaws.com/asauce-seeds/#{@product.file_path}-2")
-#     # @product.product_pictures.attach(io: file, filename: @product.file_path.concat("-2"))
-
-#     # file = open("https://s3.amazonaws.com/asauce-seeds/#{@product.file_path}-3")
-#     # @product.product_pictures.attach(io: file, filename: @product.file_path.concat("-3"))
-
-#     # file = open("https://sPres.attach(io: file, filename: @product.file_path.concat("-4"))
-#   end
-# end
-
-open("https://raw.githubusercontent.com/Aw-bree/seedHelper/master/update_products.json") do |productfiles|
+open("https://raw.githubusercontent.com/Aw-bree/seedHelper/master/asauce_upload_images.json") do |productfiles|
   data = []
 
   productfiles.read.each_line do |product|
     @item = JSON.parse(product, :quirks_mode => true)
     object = {
       "product_code": @item["product_code"],
-      "title": @item["title"],
-      "fabric_stretch": @item["fabric_stretch"],
-      "fabric_material":  @item["fabric_material"],
-      "main_fiber_content":   @item["main_fiber_content"],
-      "care_advice": @item["care_advice"],
-      "care_instructions":  @item["care_instructions"],
-      "description":  @item["description"]
+      "img_path_1": @item["img_path_1"],
+      "img_path_2": @item["img_path_2"],
+      "img_path_3": @item["img_path_3"],
+      "img_path_4": @item["img_path_4"]
     }
     data << object
   end
 
 
-  data.each do |prod|
-    code = prod[:product_code]
-    @product = Product.find_by(product_code: code)    
+  data.each do |prods|
+    code = prods[:product_code]
+    @product = Product.find_by(product_code: code)
 
-    @product.update_attributes!(title: prod[:title], fabric_stretch: prod[:fabric_stretch], fabric_material: prod[:fabric_material], main_fiber_content: prod[:main_fiber_content], care_advice: prod[:care_advice],care_instructions: prod[:care_instructions],description: prod[:description])
+    file = open("https://s3-us-west-1.amazonaws.com/asauce-seeds/asauce_photos/#{prods[:img_path_1]}")
+    @product.photos.attach(io: file, filename: prods[:img_path_1])
+
+    file = open("https://s3-us-west-1.amazonaws.com/asauce-seeds/asauce_photos/#{prods[:img_path_2]}")
+    @product.photos.attach(io: file, filename: prods[:img_path_2])
+
+    file = open("https://s3-us-west-1.amazonaws.com/asauce-seeds/asauce_photos/#{prods[:img_path_3]}")
+    @product.photos.attach(io: file, filename: prods[:img_path_3])
+
+    file = open("https://s3-us-west-1.amazonaws.com/asauce-seeds/asauce_photos/#{prods[:img_path_4]}")
+    @product.photos.attach(io: file, filename: prods[:img_path_4])
   end
 end
- 
-
