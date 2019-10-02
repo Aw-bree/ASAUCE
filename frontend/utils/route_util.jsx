4b942f8e00b1e7b5d105afc466a9322a442f5ce3
Aubrie Rice <aubrielrice@gmail.com>
2019-10-02 13:43:32 -0700

    Update route_util name and uncomment code
    route_util was previously name routes_util which was not uniform to the
    other util names. Code also contained comments from a tutorial, these
    are no longer necessary, can refer to React.js if clarity is needed.

--- frontend/utils/route_util.jsx
+++ frontend/utils/route_util.jsx
@@ -1,26 +1,11 @@
-
-
-//automate the process of allowing people to see
-// certain components based on their login
-
-
 import React from 'react';
-// these are connected componenets
 import { connect } from 'react-redux';
-// redirect to dif views, these are route specific, give 
-// diff components history, match, location, props
 import { Redirect, Route, withRouter } from 'react-router-dom';
-
-// is a user logged in?
 
 const mapStateToProps = state => ({
   loggedIn: Boolean(state.session.currentUser)
 });
 
-// pass some args directly
-// <AuthRoute path="" component={} 
-// need to make component capitalized
-// auth routes, redirect them conditions
 const Auth = ({ loggedIn, path, component: Component }) => (
   <Route
     path={path}
@@ -29,7 +14,6 @@ const Auth = ({ loggedIn, path, component: Component }) => (
     )}
   />
 );
-// logic with ternary if path matches /products kick out to login 
 
 const Protected = ({ loggedIn, path, component: Component }) => (
   <Route
@@ -40,6 +24,5 @@ const Protected = ({ loggedIn, path, component: Component }) => (
   />
 );
 
-//still need to connect it
 export const AuthRoute = withRouter(connect(mapStateToProps)(Auth));
 export const ProtectedRoute = withRouter(connect(mapStateToProps)(Protected));
