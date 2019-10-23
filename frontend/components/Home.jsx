07b34ecac97ff0eaaf539c3e366a8c94a24fe31c
Aubrie Rice <aubrielrice@gmail.com>
2019-10-22 21:10:34 -0700

    Update Home component to wait until mounted to render

--- frontend/components/Home.jsx
+++ frontend/components/Home.jsx
@@ -5,14 +5,35 @@ import HomeFeedCarousel from './HomeFeedCarousel';
 import HomeFeatureSeasonal from './HomeFeatureSeasonal';
 import HomeCategoryCarousel from './HomeCategoryCarousel';
 
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      isLoading: true
+    }
+  }
 
-export default () => (
-  <div className="home">
-    <HomeBanner />
-    <HomeFeature />
-    <HomeFeedCarousel />
-    <HomeFeatureSeasonal />
-    <HomeCategoryCarousel />
-  </div>
-);
+  componentDidMount() {
+    debugger
+    this.setState({
+      isLoading: false
+    })
+  }
+
+  render() {
+    debugger
+
+    const homeElements = (
+      <div className="home">
+        <HomeBanner />
+        <HomeFeature />
+        <HomeFeedCarousel />
+        <HomeFeatureSeasonal />
+        <HomeCategoryCarousel />
+      </div>
+    );
+
+    return this.state.isLoading ? null : homeElements
+  }
+};
